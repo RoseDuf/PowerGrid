66b2c158f3ee54c9a4a212a279793372f7c43919
Deniz Akcal <denizakcal@mail.com>
2019-02-18 18:27:20 -0500

    I added Serializable interface.

---
+++ Serializable.hpp
@@ -0,0 +1,7 @@
+#include <string>
+
+class Serializable {
+    public:  
+        virtual std::string getSerializedData(void) = 0;
+        virtual void setSerializedData(std::string serializedData) = 0;
+};
