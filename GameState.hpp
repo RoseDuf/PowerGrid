710f805a5e69e707209e03ba152753801c9e4509
Deniz Akcal <denizakcal@mail.com>
2019-03-04 19:16:24 -0500

    I (probably) finished the code for GameState class.

--- GameState.hpp
+++ GameState.hpp
@@ -1,8 +1,26 @@
 #ifndef GAMESTATE_HPP_
 #define GAMESTATE_HPP_
 
-class GameState { // TODO
-    
+#include <vector>
+#include "City.h"
+#include "player.hpp"
+
+class GameState {
+    private:
+        int turnOfPlayer;
+        std::vector< std::pair<City, City> > edgePairs;
+        std::vector<Player> players;
+    public:
+        std::vector< std::pair<City, City> > getEdgePairs();
+        void setEdgePairs(std::vector< std::pair<City, City> > edgePairs);
+        void addEdgePairs(std::vector< std::pair<City, City> > additionalEdgePairs);
+        void addEdgePair(std::pair<City, City> additionalEdgePair);
+        void setTurnOfPlayer(int turnOfPlayer);
+        int getTurnOfPlayer();
+        std::vector<Player> getPlayers();
+        void setPlayers(std::vector<Player> players);
+        void addPlayers(std::vector<Player> playersToAdd);
+        void addPlayer(Player playerToAdd);
 };
 
 #endif
