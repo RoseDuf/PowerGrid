430e4e30daaaa3fe62526d3ee2dc20d98a7327ae
icharette <35855975+icharette@users.noreply.github.com>
2019-03-06 22:46:27 -0500

    Update Elektro.cpp

--- Elektro.cpp
+++ Elektro.cpp
@@ -6,10 +6,12 @@ Elektro::Elektro()
 {
 }
 
-Elektro::Elektro(int _quantity, int _billValue)
+Elektro::Elektro(int _bill1, int _bill10, int _bill50)
 {
-	quantity = _quantity;
-	billValue = _billValue;
+
+    bills[0].second = _bill1;
+    bills[1].second = _bill10;
+    bills[2].second = _bill50;
 }
 
 Elektro::~Elektro()
@@ -17,41 +19,66 @@ Elektro::~Elektro()
 	// TO DO
 }
 
-void Elektro::collectElektro(int _quantity, int _billValue)
+
+void Elektro:: setBillAmount(int _billType, int _billAmount)
 {
-    quantity+=_quantity;
-    billValue = _billValue;
+    if(_billType==1)
+    {
+         bills[0].second = _billAmount;
+        
+    }
+    else if(_billType==10)
+    {
+        bills[1].second = _billAmount;;
+    }
+    else if(_billType==50)
+    {
+        bills[2].second = _billAmount;;
+    }
 }
 
-void Elektro::spendElektros(int _quantity, int _billValue)
+int Elektro::getBillAmmount(int _bill)
 {
-    quantity-=_quantity;
-    billValue = _billValue;
+    if(_bill==1)
+    {
+        return bills[0].second;
+        
+    }
+    else if(_bill==10)
+    {
+        return bills[1].second;
+    }
+    else
+    {
+        return bills[2].second;
+    }
+    
 }
 
-void Elektro::setElektro(int _quantity, int _billValue)
+//adds money income to wallet
+void Elektro::addElektros(int _bill1, int _bill10, int _bill50)
 {
-    quantity = _quantity;
-    billValue = _billValue;
+    bills[0].second += _bill1;
+    bills[1].second += _bill10;
+    bills[2].second += _bill50;
 }
 
-int Elektro::getQuantity()
+int Elektro::getTotalBalance()
 {
-	return quantity;
-}
-
-int Elektro::getBillValue()
-{
-	return billValue;
-}
-
-
-int Elektro::getBalance()
-{
-	return this->billValue*this->quantity;
+    int sum = 0;
+    for(int i = 0; i < bills.size() ; i++)
+    {
+        sum+=bills[i].first*bills[i].second;
+        
+    }
+    
+    return sum;
 }
 
 void Elektro::toString()
 {
-	cout << this->quantity << " :: " << this->billValue << " Elektro bills. Total of " << this->getBalance() << " Elektros." << endl;
+	cout << "Elektro " << bills[0].first << " : "  << bills[0].second << "\n" <<
+    "Elektro " << bills[1].first << " : "  << bills[1].second << "\n" <<
+    "Elektro " << bills[2].first << " : "  << bills[2].second
+    << "\nTotal of Elektros: " << this->getTotalBalance() << endl;
 }
