2b9330bd6bc89f15233a58e3bf77c21d8fd49ad4
Deniz Akcal <denizakcal@mail.com>
2019-04-09 00:40:20 -0400

    I overrode the getBiddingChoice method of Strategy that EnvironmentalistStrategy inherited.

--- EnvironmentalistStrategy.cpp
+++ EnvironmentalistStrategy.cpp
@@ -1,3 +1,19 @@
 #include "EnvironmentalistStrategy.hpp"
+#include "PowerPlant.hpp"
 
 EnvironmentalistStrategy::EnvironmentalistStrategy(AIStrategyData backgroundInformation) : Strategy(backgroundInformation) {}
+
+BiddingChoice Strategy::getBiddingChoice(Player* player, int biddedPowerPlantIndex, int highestBidSoFar) {
+    //This line should probably be elsewhere.: PowerPlant::initializePowerPlantMarket();
+    
+    bool isBidding = false;
+    
+    int amountToBid = 0;
+    
+    if( PowerPlant::peekIthPowerPlantInPresentMarket(biddedPowerPlantIndex).isGreen() && player->getTotalWallet() >= highestBidSoFar+1 ) {
+        isBidding = true;
+        amountToBid = highestBidSoFar + 1;
+    }
+    
+    return BiddingChoice(isBidding, amountToBid);
+}
