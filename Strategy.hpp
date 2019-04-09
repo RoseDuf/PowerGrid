fa1f97f92cf1a57cb622d1cb67717d633c6d67b1
Deniz Akcal <denizakcal@mail.com>
2019-04-09 00:24:22 -0400

    I changed BiddingChoice from a tuple to a pair, and I changed the type of the first element from Elektro to int.

--- Strategy.hpp
+++ Strategy.hpp
@@ -11,7 +11,7 @@
 typedef std::tuple< Market*,std::vector<Player*> > AIStrategyData;
 
 // Typedef data returned by Strategy
-typedef std::tuple<Elektro, PowerPlant*> BiddingChoice;
+typedef std::pair<int, PowerPlant*> BiddingChoice;
 typedef std::tuple<Market*, int, int, int, int> ResourcePurchaseChoice; // the ints correspond to the CHANGE from total (positive value means amount added to total, negative means amount taken away from total) in Market (0 means no change)
 
 class Strategy {
