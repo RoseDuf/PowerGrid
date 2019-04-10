d81a24235aa9eb8b859a58dd53f5c91bdea8079e
Elsa <ka1a@protonmail.com>
2019-04-10 10:56:18 -0400

    fixed pointer problem, everything Builds Correctly!

--- main.cpp
+++ main.cpp
@@ -279,12 +279,13 @@ int main() {
 	PowerPlant *p38 = new PowerPlant(40, 6, 0, 2, 0, 0);
 	PowerPlant *p39 = new PowerPlant(42, 6, 2, 0, 0, 0);
 
-	players[0]->addPowerPlant(*p1);
-	players[1]->addPowerPlant(*p29);
-	players[1]->addPowerPlant(*p35);
-	players[2]->addPowerPlant(*p36);
-	players[3]->addPowerPlant(*p37);
-
+	//changed the p1 from *p1 to p1 [there was a LINKER PROBLEM] resolved now
+	players[0]->addPowerPlant(p1);
+	players[1]->addPowerPlant(p29);
+	players[1]->addPowerPlant(p35);
+	players[2]->addPowerPlant(p36);
+	players[3]->addPowerPlant(p37);
+	
 
 
 	//Game loop !!!!
