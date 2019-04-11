a96e5aee6e6ca4a391d9a1b7e2fbe9df95797b61
Deniz Akcal <denizakcal@mail.com>
2019-04-10 21:14:59 -0400

    I removed the inline keyword from combineVectors function in HelperFunctions.hpp file.

--- HelperFunctions.hpp
+++ HelperFunctions.hpp
@@ -24,7 +24,7 @@ namespace HelperFunctions {
     
     bool equalsIgnoreCase(std::string str1, std::string str2);
     
-    template<typename T> inline std::vector<T> combineVectors(std::vector<T> v1, std::vector<T> v2) {
+    template<typename T> std::vector<T> combineVectors(std::vector<T> v1, std::vector<T> v2) {
         std::vector<T> combination;
         for(int i = 0; i < v1.size(); i++) {
             combination.push_back( v1.at(i) );
