d1397457d646e2df10932b40a05e129c7e0d20a9
Deniz Akcal <denizakcal@mail.com>
2019-04-11 21:17:36 -0400

    I added comment for EnvironmentalistStrategy's virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep);.

--- EnvironmentalistStrategy.hpp
+++ EnvironmentalistStrategy.hpp
@@ -8,7 +8,7 @@ class EnvironmentalistStrategy : public Strategy {
     public:
         EnvironmentalistStrategy(AIStrategyData backgroundInformation);
         virtual BiddingDetails getBiddingDetails(const Player* player, int biddedPowerPlantIndex, int highestBigSoFar);
-        virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep);
+        virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep); // aims to occupy exactly the amount of cities that owned power plants can power in the cheapest way possible
 };
 
 #endif
