c798e1177bbb986b68b9881282054acbd7a226e3
Deniz Akcal <denizakcal@mail.com>
2019-04-11 23:37:38 -0400

    I fixed merging conflict from popping my stash.

--- ModerateStrategy.hpp
+++ ModerateStrategy.hpp
@@ -7,7 +7,7 @@
 class ModerateStrategy : public Strategy {
     public:
         ModerateStrategy(AIStrategyData backgroundInformation);
-        virtual BiddingDetails getBiddingDetails(const Player* player, int biddedPowerPlantIndex, int highestBigSoFar); // if this player has enough resources to fully power pps and has less than avg player's elektros, then gets aggressive for buying best pp possible
+        virtual BiddingDetails getBiddingDetails(const Player* player, int biddedPowerPlantIndex, int highestBigSoFar); // If this player has enough resources to fully power pps and has less than avg player's elektros, then gets aggressive for buying best pp possible
         virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep);
 };
 
