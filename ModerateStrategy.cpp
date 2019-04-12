cb9db0278f83811d3cd6a2f55df69ed5d6458022
Deniz Akcal <denizakcal@mail.com>
2019-04-12 04:03:22 -0400

    I have a version of resource purchasing method for ModerateStrategy working that assumes player has enough elektros.

--- ModerateStrategy.cpp
+++ ModerateStrategy.cpp
@@ -102,5 +102,13 @@ std::vector<City> ModerateStrategy::getCityBuildingChoice(const Player* player, 
 }
 
 ResourcePurchaseChoice ModerateStrategy::getResourcePurchaseChoice(const Player* player) {
-    return ResourcePurchaseChoice(0,0,0,0); // TODO
+    
+    // resources in alphabetical order (coal, garbage, oil, uranium)
+    
+    int coalsToPurchase = player->getTotalCoalStockable() - player->getTotalCoalStocked();
+    int garbagesToPurchase = player->getTotalGarbageStockable() - player->getTotalGarbageStocked();
+    int oilsToPurchase = player->getTotalOilStockable() - player->getTotalOilStocked();
+    int uraniumsToPurchase = player->getTotalUraniumStockable() - player->getTotalUraniumStocked();
+    
+    return ResourcePurchaseChoice(coalsToPurchase,garbagesToPurchase,oilsToPurchase,uraniumsToPurchase);
 }
