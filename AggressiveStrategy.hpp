cbe30cc6b228f545baa0a3a1688e208f5bdca70a
Deniz Akcal <denizakcal@mail.com>
2019-04-11 21:16:15 -0400

    I added comment for AggressiveStrategy's virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep);.

--- AggressiveStrategy.hpp
+++ AggressiveStrategy.hpp
@@ -8,7 +8,7 @@ class AggressiveStrategy : public Strategy {
     public:
         AggressiveStrategy(AIStrategyData backgroundInformation);
         virtual BiddingDetails getBiddingDetails(const Player* player, int biddedPowerPlantIndex, int highestBigSoFar);
-        virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep);
+        virtual std::vector<City> getCityBuildingChoice(const Player* player, int gameStep); // aims to occupy one more city than power plants can power in the cheapest way possible
 };
 
 #endif
